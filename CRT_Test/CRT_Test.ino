6ec22982369bdd087c78bbf6ac51ad44bc1e9b7c
ericgonza <mgonzalez35@gmail.com>
2018-01-28 14:44:28 -0800

    test1

---
+++ CRT_Test/CRT_Test.ino
@@ -0,0 +1,20 @@
+#include <TVout.h>
+//2nd test
+ 
+TVout TV;
+ 
+void setup () {
+  TV.start_render( _NTSC );
+}
+ 
+void loop () {
+  TV.clear_screen ();
+  TV.print_str (0, 0, "All your TV");
+  TV.print_str (0, 10, "are belong");
+  TV.print_str (0, 20, "to us!!!");
+ 
+  TV.delay (60);
+}
+
+//2nd test 2
+//4th
